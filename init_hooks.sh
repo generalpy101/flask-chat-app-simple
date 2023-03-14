c5da636131e869cf042897054613d8979bfed0a1
Prakash Yogi <prakash.y@fyle.in>
2023-03-14 23:44:25 +0530

    made changes in init_hooks.sh to use printf instead of echo

--- init_hooks.sh
+++ init_hooks.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-echo $"#/bin/sh\nblack ./src\npython lint.py -p ./src'> .git/hooks/pre-commit"
+
+printf "#/bin/sh\nblack ./src\npython lint.py -p ./src" > .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
