e65e035b28f3eeee84ead07bcbe7755c171e8ab7
Prakash Yogi <prakash.y@fyle.in>
2023-03-15 09:03:03 +0530

    Changed formnatting in wsgi file

--- src/wsgi.py
+++ src/wsgi.py
@@ -2,4 +2,6 @@ from main import socketio, app
 import os
 
 if __name__ == "__main__":
-    socketio.run(app, "0.0.0.0",port=os.environ.get("FLASK_SERVER_PORT") or 5000, debug = True)
+    socketio.run(
+        app, "0.0.0.0", port=os.environ.get("FLASK_SERVER_PORT") or 5000, debug=True
+    )
